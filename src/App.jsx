6936cc3c779be0ee6e3b229a40a30002b8751943
Asttttha <asthas2003@gmail.com>
2024-02-08 14:56:00 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar"
 import './App.css'
 import Footer from "./components/Footer"
+import CardDiv from "./components/Cards/CardDiv"
 
 export default function App() {
   return (
     <>
     <Navbar/>
+    <CardDiv/>
+    <CardDiv/>
     <Footer/>
-
+    
    
     </>
   )
