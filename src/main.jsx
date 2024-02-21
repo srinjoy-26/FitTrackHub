aa055babeb2b14c1e0bacbe91b89f30497142bb4
srinjoy-26 <srinjoydev26@gmail.com>
2024-02-22 00:11:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router} from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
